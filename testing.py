de9f318ba5b3387bbb29c86459b42d1bf253dd5f
Timon <spiondroide@gmail.com>
2022-01-26 22:59:21 +0100

    Began MIT Opencourseware Python Basics Course

---
+++ testing.py
@@ -0,0 +1,16 @@
+# hi = "Hello there"
+# name = "timon"
+# greet = hi + " " + name
+# print(greet) 
+
+
+#x = 1
+#x_str = str(x)
+#y = 2
+#y_str = str(y) # ist nicht nötig mit Kommas 
+#print("My favourite number is",x,y) # Kommas bringen automatisch Leerzeichen in den String
+#print("My favourite number is"+x_str+y_str) # Plus ist immer explizit, ohne Leerzeichen
+
+#text = input("Give me a string") # Takes a user input, default: string
+#print(text*2) # Prints an input n times -> Works on strings
+
