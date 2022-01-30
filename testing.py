d62a385130f506ae3dac54a8d85757a81e1407a8
Timon <spiondroide@gmail.com>
2022-01-30 23:52:46 +0100

    Advanced in the python course

--- testing.py
+++ testing.py
@@ -1,16 +1,41 @@
+## Strings in Python
+
 # hi = "Hello there"
 # name = "timon"
 # greet = hi + " " + name
 # print(greet) 
 
 
-#x = 1
-#x_str = str(x)
-#y = 2
-#y_str = str(y) # ist nicht nötig mit Kommas 
-#print("My favourite number is",x,y) # Kommas bringen automatisch Leerzeichen in den String
-#print("My favourite number is"+x_str+y_str) # Plus ist immer explizit, ohne Leerzeichen
+# x = 1
+# x_str = str(x)
+# y = 2
+# y_str = str(y) # ist nicht nötig mit Kommas 
+# print("My favourite number is",x,y) # Kommas bringen automatisch Leerzeichen in den String
+# print("My favourite number is"+x_str+y_str) # Plus ist immer explizit, ohne Leerzeichen
 
-#text = input("Give me a string") # Takes a user input, default: string
-#print(text*2) # Prints an input n times -> Works on strings
+# text = input("Give me a string") # Takes a user input, default: string
+# print(text*2) # Prints an input n times -> Works on strings
 
+## Boolean Operators, Compare Functions
+
+# a = 'A'
+
+# if(a > 1):
+#    print('Yeah, sure, buddy')
+
+# Python can't compare strings and numbers
+
+# x == y Equality test, TRUE if x is the same as y
+# x != y Inequality test
+# Python implements 'not', 'and', 'or'
+
+# def conditionchecker(con):
+#     if(con == 1):
+#         print('aye')
+#     elif(con == 2):
+#         print('no')
+#     else:
+#         return -1
+
+# test = input('Check with me:')
+# conditionchecker(int(test)) 
